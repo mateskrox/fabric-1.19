f8f1951ada1ed379e61aa44c16e9bbaefadbe704
mateskrox <mateofiorotto2004@gmail.com>
2022-09-04 04:38:07 -0300

    Initial commit

---
+++ src/main/java/net/mateskrox/modtest/ModTest.java
@@ -0,0 +1,15 @@
+package net.mateskrox.modtest;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ModTest implements ModInitializer {
+	public static final String MOD_ID = "modtest";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mateskrox/modtest/ModTestClient.java
@@ -0,0 +1,11 @@
+package net.mateskrox.modtest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModTestClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
